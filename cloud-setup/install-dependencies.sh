cea06bc1d6bf6d5d3f5fc7102e8bb5d149334401
Celebrum <jeansebastienbeaulieuscrde.01@gmail.com>
2025-03-17 22:07:58 -0400

    Add Docker and Kubernetes setup scripts with Docker Compose configuration

---
+++ cloud-setup/install-dependencies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update system
+sudo yum update -y
+
+# Install Docker
+sudo yum install -y docker-engine
+
+# Start and enable Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Add current user to docker group
+sudo usermod -aG docker $USER
+
+# Install kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+sudo yum install -y kubectl
+
+# Install minikube for local kubernetes
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
